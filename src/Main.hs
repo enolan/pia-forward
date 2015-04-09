987b002fe75d50c771aa3ffcb3cd1b8aba111548
Echo Nolan <echo@echonolan.net>
2015-04-09 05:34:25 -0700

    Read and write config files

--- src/Main.hs
+++ src/Main.hs
@@ -3,15 +3,30 @@ module Main (main) where
 import Control.Concurrent
 import Control.Exception
 import Control.Monad
+import Data.Aeson
 import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as LB
 import Data.Default
 import Data.Functor
-import Data.List.Extra
+import Data.List.Extra (find, trim)
 import Data.Maybe
+import Data.Monoid (mconcat)
+import Data.Text (Text)
+import Data.Text.Format (left)
+import Data.Text.Format.Types (Hex(..))
+import Data.Text.IO (getLine)
+import Data.Text.Lazy (toStrict)
+import Data.Text.Lazy.Builder
+import Data.Word (Word8)
+import GHC.Generics
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.Info
+import System.Directory (createDirectoryIfMissing)
+import System.Environment.XDG.BaseDir (getUserConfigFile)
+import System.FilePath (takeDirectory)
 import System.Process
+import System.Random
 
 main = do
     clientId <- trim <$> readFile "/home/enolan/.pia_client_id"
@@ -36,3 +51,35 @@ main = do
             Left (ex :: SomeException) -> print ex
             Right _ -> return ()
         threadDelay $ 30*60*1000000
+
+setupNewConfig :: IO Config
+setupNewConfig = do
+    bytes :: [Word8] <- replicateM 16 randomIO
+    let clientId =
+            toStrict $ toLazyText $ mconcat $ map (left 2 '0' . Hex) bytes
+    putStrLn "Enter PIA username:"
+    user <- Data.Text.IO.getLine
+    putStrLn "Enter PIA password:"
+    pass <- Data.Text.IO.getLine
+    let cfg = Config {clientId, user, pass}
+    writeConfig cfg
+    return cfg
+
+data Config =
+    Config {clientId :: Text, user :: Text, pass :: Text}
+    deriving (Show, Generic)
+
+instance FromJSON Config
+instance ToJSON Config
+
+getConfigPath :: IO FilePath
+getConfigPath = getUserConfigFile "pia-forward" "config"
+
+writeConfig :: Config -> IO ()
+writeConfig cfg = do
+    configPath <- getConfigPath
+    createDirectoryIfMissing True $ takeDirectory configPath
+    LB.writeFile configPath (encode cfg)
+
+readConfig :: IO Config
+readConfig = (fromJust . decode) <$> (getConfigPath >>= LB.readFile)
